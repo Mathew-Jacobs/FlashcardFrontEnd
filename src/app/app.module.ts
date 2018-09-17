d7c79d69985d2276d317b64fb22b495bc01bb027
Merideth Bush <meridethbush@gmail.com>
2018-09-17 15:19:15 -0400

    step 5 routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
 import { MatToolbarModule } from '@angular/material';
 import {MatButtonModule} from '@angular/material/button';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { RegistrationComponent } from './components/registration/registration.component';
+
+const routes = [
+  { path: 'register', component: RegistrationComponent }
+];
 
 @NgModule({
   declarations: [
@@ -17,6 +23,7 @@ import { RegistrationComponent } from './components/registration/registration.co
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot(routes),
     MatToolbarModule,
     MatButtonModule 
   ],
