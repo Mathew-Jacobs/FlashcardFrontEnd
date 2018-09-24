95d878528da0beaedd89f2bb2a51c97219d5290f
Mathew-Jacobs <mathew.jacobs.exe@gmail.com>
2018-09-24 10:58:18 -0400

    dialog box

--- src/app/components/deck/deck-delete/deck-delete.component.ts
+++ src/app/components/deck/deck-delete/deck-delete.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { DecksService } from '../../../services/decks.service';
 import { ActivatedRoute, Router } from '../../../../../node_modules/@angular/router';
 import { Deck } from '../../../models/Deck';
+import { MAT_DIALOG_DATA } from '../../../../../node_modules/@angular/material';
 
 @Component({
   selector: 'app-deck-delete',
@@ -12,12 +13,11 @@ export class DeckDeleteComponent implements OnInit {
 
   deck: Deck;
 
-  constructor(private _deckService: DecksService, private _ar: ActivatedRoute, private _router: Router) { 
-    this._ar.paramMap.subscribe(p => {
-      this._deckService.getDeck(p.get('id')).subscribe((singleDeck: Deck) => {
+  constructor(private _deckService: DecksService, @Inject(MAT_DIALOG_DATA) public data: any, private _router: Router) { 
+    console.log(data);
+      this._deckService.getDeck(data.id).subscribe((singleDeck: Deck) => {
         this.deck = singleDeck;
       });
-    });
   }
 
   ngOnInit() {
