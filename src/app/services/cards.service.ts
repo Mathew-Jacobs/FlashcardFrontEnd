de9ce94a78dcd2b64136b591a482aa42cf605495
Jenna Pearmain <ravynskyes88@gmail.com>
2018-09-27 16:14:03 -0400

    saving

--- src/app/services/cards.service.ts
+++ src/app/services/cards.service.ts
@@ -13,7 +13,7 @@ export class CardsService {
   constructor(private _http: HttpClient) { }
 
   getCards(did: string) {
-    return this._http.get(`${Api_Url}api/Flashcard?did=${did}`, { headers: this.getHeaders() });
+    return this._http.get(`${ApiUrl}api/Flashcard?did=${did}`, { headers: this.getHeaders() });
   }
 
   private getHeaders() {
@@ -21,18 +21,18 @@ export class CardsService {
   }
 
   createCard(card: Card) {
-    return this._http.post(`${Api_Url}api/Flashcard`, card, { headers: this.getHeaders() });
+    return this._http.post(`${ApiUrl}api/Flashcard`, card, { headers: this.getHeaders() });
   }
 
   getCard(id: string, did: string) {
-    return this._http.get(`${Api_Url}api/Flashcard/${id}?did=${did}`, { headers: this.getHeaders() });
+    return this._http.get(`${ApiUrl}api/Flashcard/${id}?did=${did}`, { headers: this.getHeaders() });
   }
 
   updateCard(card: Card) {
-    return this._http.put(`${Api_Url}api/Flashcard`, card,{headers: this.getHeaders()})
+    return this._http.put(`${ApiUrl}api/Flashcard`, card,{headers: this.getHeaders()})
   }
 
   deleteCard(id: number){
-    return this._http.delete(`${Api_Url}api/Flashcard/${id}`, { headers: this.getHeaders()});
+    return this._http.delete(`${ApiUrl}api/Flashcard/${id}`, { headers: this.getHeaders()});
   }
 }
--- src/app/services/decks.service.ts
+++ src/app/services/decks.service.ts
@@ -11,23 +11,23 @@ export class DecksService {
   constructor(private _http: HttpClient) { }
 
   getDecks() {
-    return this._http.get(`${Api_Url}api/Deck`, { headers: this.getHeaders() });
+    return this._http.get(`${ApiUrl}api/Deck`, { headers: this.getHeaders() });
   }
 
   createDeck(deck: Deck) {
-    return this._http.post(`${Api_Url}api/Deck`, deck, {headers: this.getHeaders()});
+    return this._http.post(`${ApiUrl}api/Deck`, deck, {headers: this.getHeaders()});
   }
 
   getDeck(id: string) {
-    return this._http.get(`${Api_Url}api/Deck/${id}`, { headers: this.getHeaders()});
+    return this._http.get(`${ApiUrl}api/Deck/${id}`, { headers: this.getHeaders()});
   }
 
   updateDeck(deck: Deck) {
-    return this._http.put(`${Api_Url}api/Deck`, deck, { headers: this.getHeaders()});
+    return this._http.put(`${ApiUrl}api/Deck`, deck, { headers: this.getHeaders()});
   }
 
   deleteDeck(id: number) {
-    return this._http.delete(`${Api_Url}api/Deck/${id}`, { headers: this.getHeaders() });
+    return this._http.delete(`${ApiUrl}api/Deck/${id}`, { headers: this.getHeaders() });
   }
 
   private getHeaders() {
